6c73d78f6480ab2c5683449a8cdc86019d810bef
Cheng <123456789@qq.com>
2019-08-01 14:29:32 +0800

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var result = JsonConvert.SerializeObject(new { Id = 1, Name = "小明" });
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
